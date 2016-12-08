7b808a76dc99815c4160203ecf9b7500c7bbde0a
spirkey <stevie1013@gmail.com>
2016-12-08 10:39:28 -0500

    Added some more test cases to Scrape

--- scrapeTest.java
+++ scrapeTest.java
@@ -9,6 +9,19 @@ public class scrapeTest {
 	public static void main(String[] args) {
 	Scrape test = new Scrape();
 	System.out.println(test.webScrape("run"));//test in order to make sure webscrape is working properly
+	System.out.println(test.webScrape("walk"));//test in order to make sure webscrape is working properly
+	System.out.println(test.webScrape("sprint"));//test in order to make sure webscrape is working properly
+	System.out.println(test.webScrape("house"));//test in order to make sure webscrape is working properly
+	System.out.println(test.webScrape("school"));//test in order to make sure webscrape is working properly
+	System.out.println(test.webScrape("bicycle"));//test in order to make sure webscrape is working properly
+	System.out.println(test.webScrape("fog"));//test in order to make sure webscrape is working properly
+	System.out.println(test.webScrape("computer"));
+	System.out.println(test.webScrape("laptop"));
+	System.out.println(test.webScrape("screen"));
+	System.out.println(test.webScrape("mirror"));
+
+	
+	
 	
 	System.out.println("\n\n\n--------------------------");
 	
