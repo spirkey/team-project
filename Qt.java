17f76fa4ee5da26a94d9f270a6393b952032c8ac
spirkey <stevie1013@gmail.com>
2016-12-08 11:31:15 -0500

    updated qt for automation

--- Qt.java
+++ Qt.java
@@ -1,13 +1,26 @@
 public class Qt {
-		 public static void main(String[] args) {
 		 
+	public static void main(String[] args) {
+		 
+		 // here question picks the correct answer
+		 System.out.println("CORRECT TEST");
 		 Question q = new Question("what color is an orange?", "green", "blue", "orange", "purple", "orange");
 		 System.out.println(q.getQuestion());
-		 System.out.println("Enter choice: ");
-		 java.util.Scanner s = new java.util.Scanner(System.in);
-		 String pick = s.next();
-		 q.pick(pick);
+		 q.pick(q.getC()); // pick orange
+		 System.out.println("you picked: " + q.getChoice());
 		 System.out.println(q.isRight());
-		 System.out.println(q.getCorrectChoice());
+		 System.out.println("correct was: " + q.getCorrectChoice());
+		 System.out.println(q.printResults());
+		 
+		 System.out.println("--------------------");
+		 
+		 // here question picks the wrong answer
+		 System.out.println("INCORRECT TEST");
+		 System.out.println(q.getQuestion());
+		 q.pick(q.getA()); // pick green
+		 System.out.println("you picked: " + q.getChoice());
+		 System.out.println(q.isRight());
+		 System.out.println("correct was: " + q.getCorrectChoice());
+		 System.out.println(q.printResults());
 	}
 }
